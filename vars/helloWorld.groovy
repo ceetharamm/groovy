7b9e70e9edfb7bb0baf5b76222222558aee58bc7
Seetharamulu Maddipati <ceetharamm@gmail.com>
2024-02-22 21:24:12 -0500

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,14 @@
 def call() {
   sh 'echo hi from DevOps team. This is shared library example.'
 }
+
+// Deploy war file into multiple servers
+// In Jenkinsfile create a variable staging_server and add servers in comma separated
+def war(){
+  echo "Deploying War file"
+}
+
+//Deploy react application
+def react(){
+  echo "Deploying React Application"
+}
